55e951adec48e36620d54a0c3a6b477d372998e5
Retzudo <christoph.willinger@gmail.com>
2015-04-23 09:39:32 +0200

    Okaay then...

--- taskstack/core/admin.py
+++ taskstack/core/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 from core.models import Member, Queue, Task
-from guardian.admin import GuardedModelAdmin
 
 
-class MemberAdmin(GuardedModelAdmin):
-    pass
-
-
-class QueueAdmin(GuardedModelAdmin):
-    pass
-
-
-admin.site.register(Member, MemberAdmin)
-admin.site.register(Queue, QueueAdmin)
+admin.site.register(Member)
+admin.site.register(Queue)
 admin.site.register(Task)
